0658a8314ddf8ef19b0b619df5f1506f80bb516e
FakeModz <78726008+FakeModz@users.noreply.github.com>
2025-05-07 16:21:06 +0700

    Create PotionList.lua

---
+++ PotionList.lua
@@ -0,0 +1,37 @@
+return {
+	LuckyList = {
+		"Lucky I", 
+		"Lucky II", 
+		"Lucky III", 
+		"Lucky IV", 
+		"Lucky V", 
+		"Lucky Evolved"
+	},
+
+	SpeedList = {
+		"Speed I", 
+		"Speed II", 
+		"Speed III", 
+		"Speed IV", 
+		"Speed V", 
+		"Speed Evolved"
+	},
+
+	MythicList = {
+		"Mythic I", 
+		"Mythic II", 
+		"Mythic III", 
+		"Mythic IV", 
+		"Mythic V", 
+		"Mythic Evolved"
+	},
+
+	ListCoinP = {
+		"Coins I", 
+		"Coins II", 
+		"Coins III", 
+		"Coins IV", 
+		"Coins V", 
+		"Coins Evolved"
+	}
+}
