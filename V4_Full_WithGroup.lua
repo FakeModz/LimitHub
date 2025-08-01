c0538fb511e21024d1bf295bbc2693d6c4b1f16a
FakeModz <78726008+FakeModz@users.noreply.github.com>
2025-08-01 13:59:57 +0700

    Update V4_Full_WithGroup.lua

--- V4_Full_WithGroup.lua
+++ V4_Full_WithGroup.lua
@@ -1,6 +1,6 @@
 
 
---V13.1
+--V13.2
 
 local Lighting = game:GetService("Lighting")
 local RunService = game:GetService("RunService")
@@ -124,8 +124,8 @@ local function CloseOpen()
 	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 	Close_ImageButton.Parent = ScreenGui
-	Close_ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 139)
-	Close_ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 139)
+	Close_ImageButton.BackgroundColor3 = Color3.fromRGB(10, 10, 40)
+	Close_ImageButton.BorderColor3 = Color3.fromRGB(25, 25, 112)
 	Close_ImageButton.Position = UDim2.new(0.1021, 0, 0.0743, 0)
 	Close_ImageButton.Size = UDim2.new(0, 59, 0, 49)
 	Close_ImageButton.Image = "rbxassetid://123767073052336"
